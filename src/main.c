0557f1a3932cc1745b233c6a3ff3b3e677b8383c
dan <danjedwards01@gmail.com>
2024-10-27 19:22:15 +0000

    Debugging working with arduino uno board

--- src/main.c
+++ src/main.c
@@ -1,18 +1,23 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
+#include "avr8-stub.h"
 
 void blink(void)
 {
-    DDRB |= (1 << PB0); // Set pin 0 on port B as output
+    DDRB |= (1 << PB5); // Set pin 0 on port B as output
     while (1)
     {
-        PORTB ^= (1 << PB0); // Toggle the pin
-        _delay_ms(1);        // Delay for 500ms
+        PORTB ^= (1 << PB5); // Toggle the pin
+        // _delay_ms(1);        // Delay for 500ms
     }
 }
 
 int main()
 {
+    debug_init();
+    sei();
+    // breakpoint();
     blink();
     return 0;
 }
